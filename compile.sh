096f0bced632d24c55e3a1166af354c2c80a8dfc
TheSenatorSteven <36710396+TheSenatorSteven@users.noreply.github.com>
2024-01-22 17:39:50 +0200

    Add files via upload

--- compile.sh
+++ compile.sh
@@ -22,10 +22,37 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-#!/bin/sh
+#!/bin/bash
 
-if [ ! -d "output" ]; then
-	mkdir output
-fi
-gcc -Wall -Wextra -pedantic -std=c99 globals.c microrun.c getParameters.c readConfig.c eventLoop.c -lX11 -o output/microrun
+DEBUG=true
+
+name="microrun"
+asm="output/asm"
+libraries="-lxcb -lxcb-randr"
+
+compileFile(){
+	gcc -x c \
+	    -S \
+	    -std=c89 \
+	    -Os \
+	    -Waddress -Warray-bounds=1 -Wbool-compare -Wbool-operation -Wchar-subscripts -Wduplicate-decl-specifier -Wformat -Wformat-overflow -Wformat-truncation -Wint-in-bool-context -Wimplicit -Wimplicit-int -Wimplicit-function-declaration -Winit-self -Wlogical-not-parentheses -Wmain -Wmaybe-uninitialized -Wmemset-elt-size -Wmemset-transposed-args -Wmissing-attributes -Wmissing-braces -Wmultistatement-macros -Wnarrowing -Wnonnull -Wnonnull-compare -Wopenmp-simd -Wparentheses -Wpointer-sign -Wrestrict -Wreturn-type -Wsequence-point -Wsign-compare -Wsizeof-pointer-div -Wsizeof-pointer-memaccess -Wstrict-aliasing -Wstrict-overflow=1 -Wswitch -Wtautological-compare -Wtrigraphs -Wuninitialized -Wunknown-pragmas -Wunused-function -Wunused-label -Wunused-value -Wunused-variable -Wvolatile-register-var \
+	    -Wclobbered -Wcast-function-type -Wempty-body -Wignored-qualifiers -Wimplicit-fallthrough=3 -Wmissing-field-initializers -Wmissing-parameter-type -Wold-style-declaration -Woverride-init -Wsign-compare -Wtype-limits -Wuninitialized -Wshift-negative-value -Wunused-parameter -Wunused-but-set-parameter \
+	    -Wpedantic \
+	    $libraries \
+	    -D_POSIX_C_SOURCE=200112L \
+	    -o "$asm/$1.s" "source/$1.cold"
+	gcc "$asm/$1.s" $libraries -o output/$name
+	return
+}
+
+cd $(dirname $0)
+[ ! -f compile.sh ] && { cd $(cd $(dirname $BASH_SOURCE) && pwd);                } || :
+[ ! -f compile.sh ] && { printf "could not find compile.sh directory\n"; exit 0; } || :
+[   -d output     ] && { rm -r output;                                           } || :
+mkdir output $asm
+[ ! -d $asm       ] && { printf "could not write executable to disk\n";  exit 1; } || :
+compileFile $name
+[ $DEBUG = false  ] && { rm -r $asm;                                             } || :
+printf "#!/bin/bash\n\ncd \$(dirname \$0)\n[ ! -f $name ] && cd \$(cd \$(dirname \$BASH_SOURCE) && pwd) || :\n./$name \"\$@\"\nexit 0\n" > output/run
+chmod +x output/run
 exit 0
